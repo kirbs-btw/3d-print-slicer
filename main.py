cc5c0b12383563248561e7a75c28657b77dc7ce2
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2023-08-04 12:56:52 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import slicer_lib
+
+
+def main():
+    """
+    
+    
+    """
+    file_path = ""
+    layer_hight = 0.2 # in mm
+    x_dim = 100 
+    y_dim = 100
+    z_dim = 100
+
+    
+
+
+
+
+    pass
+
+if __name__ == '__main__':
+    main()
