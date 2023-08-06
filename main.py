9bf221ad44f7cc6f545018eb982ed7ca7d39b865
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2023-08-06 13:49:40 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import slicer_lib
-
+from slicer_lib import get_point_slice as sp
+from stl import mesh
 
 def main():
     """
     
     
     """
-    file_path = ""
+    file_path = 'H:/Projekte/Projekte/Project 137/3d-print-slicer/demo_stl_files/cube.stl'
     layer_hight = 0.2 # in mm
     x_dim = 100 
     y_dim = 100
     z_dim = 100
 
+    stl_obj = mesh.Mesh.from_file(file_path)
+    points = sp.get_points_from_stl(stl_obj)
     
 
 
