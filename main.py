277c224b6cc58c54efff9b68ee85be3ebd7cbb82
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2023-10-31 17:11:34 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,33 +1,20 @@
-import slicer_lib
-from slicer_lib import get_point_slice as sp
-from slicer_lib import get_gcode_from_points as cg
 from stl import mesh
 
-def save_gcode(gcode, save_path="", file_name="unnamend"):
-    file = open('{}{}.gcode'.format(save_path, file_name), 'w+')
-    for line in gcode:
-        file.write('\n{}'.format(line))
-
 def main():
-    """
-    the method of filling flore and bottom are way to inefficient
-    lowerd the numbers in flore and bottom count 
-    im confused with the changed numberse but the distances are still ok idk how it works 
-    """
-    file_path = 'H:/Projekte/Projekte/Project 137/3d-print-slicer/demo_stl_files/cube.stl'
+    file_path = ''
     layer_hight = 0.2 # in mm
-    x_dim = 50
-    y_dim = 50
-    z_dim = 50
-    plate_shift = 30
-
-    file_name = 'test_cube_with_bottomandtop'
-    save_path = 'H:/Projekte/Projekte/Project 137/3d-print-slicer/save/'
+    obj_x_dim = 50 # in mm
+    obj_y_dim = 50 # in mm
+    obj_z_dim = 50 # in mm
     
+    x_plate_offset = 50
+    y_plate_offset = 50
+
+    file_name = ''
+    save_path = ''
+
     stl_obj = mesh.Mesh.from_file(file_path)
-    points = sp.get_points_from_stl(stl_obj, layer_hight=layer_hight, x_dim=x_dim, y_dim=y_dim, z_dim=z_dim, offset=plate_shift)
-    gcode = cg.create_gcode(points=points)
-    save_gcode(gcode, save_path=save_path, file_name=file_name)
+    
 
 if __name__ == '__main__':
     main()
