90b7d801459cb4b5bb0466c49b0697913ccfa016
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2023-11-05 22:26:46 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from stl import mesh
+from slicer_lib import stl_to_lines as stltl
+from slicer_lib import lines_to_points as ltp
 
 def main():
+    printer_x = 2000
+    printer_y = 2000
+    printer_z = 2000
+    
     file_path = ''
     layer_hight = 0.2 # in mm
     obj_x_dim = 50 # in mm
@@ -14,7 +20,8 @@ def main():
     save_path = ''
 
     stl_obj = mesh.Mesh.from_file(file_path)
+    line_triangles = stltl.get_points_from_stl(stl_obj, obj_x_dim=obj_x_dim, obj_y_dim=obj_y_dim, obj_z_dim=obj_z_dim, x_plate_offset=x_plate_offset, y_plate_offset=y_plate_offset)
+    points = ltp.lines_to_points(line_triangles, layer_hight)
     
-
 if __name__ == '__main__':
     main()
