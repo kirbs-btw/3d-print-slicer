59b239340d7907aec6840e5e423a6ca19b99ae72
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2023-11-09 12:58:16 +0100

    Add files via upload
    outline sorted

--- slicer_lib/lines_to_points.py
+++ slicer_lib/lines_to_points.py
@@ -35,7 +35,71 @@ def slice_z(line_triangles, layer_height, layer_count_z):
 
 
     return layer_point_pairs
+
+def points_are_equal(point_a, point_b):
+    """
+    checks is points are equal 
+    function exists to match up lines in sort_pairs()
+    """
+
+    if point_a[0] != point_b[0]:
+        return False
+    if point_a[1] != point_b[1]:
+        return False
     
+    """
+    not needed because we check only points that are on the same layer
+
+    if point_a[2] == point_b[2]:
+        return False
+    """
+
+    return True
+
+def point_is_equal_nrml(pair_a, pair_b):
+    return points_are_equal(pair_a[-1], pair_b[0])
+
+def point_is_equal_swap(pair_a, pair_b):    
+    return points_are_equal(pair_a[-1], pair_b[-1])
+
+def find_fitting_pair(checking_pair, pair_list):
+    for index, pair in enumerate(pair_list):
+        if pair == [] or checking_pair == []:
+            continue
+        if point_is_equal_nrml(checking_pair, pair):
+            return [pair, index]
+        if point_is_equal_swap(checking_pair, pair):
+            new_pair = [pair[1], pair[0]]
+            return [new_pair, index]
+
+def sort_layer_pairs(layer_pairs):
+    layer_sorted = []
+
+    layer_pairs_save = layer_pairs # saves unsorted state of layer_pairs to be changes later
+    while layer_pairs_save != []: 
+        run_time = len(layer_pairs_save)
+        sorted_element = [layer_pairs_save[0]] # sorted pairs
+        layer_pairs_save.remove(sorted_element[0])
+        for _ in range(run_time):
+            fitting = find_fitting_pair(sorted_element[-1], layer_pairs_save)
+            if fitting == None:
+                continue
+            fitting_pair = fitting[0]
+            index_of_fitting_pair = fitting[1]
+            sorted_element.append(fitting_pair)
+            layer_pairs_save.remove(layer_pairs_save[index_of_fitting_pair])
+        layer_sorted.append(sorted_element)
+
+    return layer_sorted
+
+def sort_point_pairs(layer_point_pairs):
+    point_pairs_sorted = []
+    
+    for layer in layer_point_pairs:
+        point_pairs_sorted.append(sort_layer_pairs(layer))
+    
+    return point_pairs_sorted
+
 
 
 def lines_to_points(line_triangles, layer_height, obj_z_height):
