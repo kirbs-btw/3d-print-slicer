be08fc809c28c393a722c47089c658ef5b8f354a
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2023-11-12 17:03:33 +0100

    Add files via upload
    adding infill function

---
+++ slicer_lib/add_infill.py
@@ -0,0 +1,17 @@
+from Line import *
+
+def cross_lines(obj_size_x = 10, obj_size_y = 10, obj_size_z = 10, spacing = 0.5):
+    cross_lines = []
+    
+    return cross_lines
+    
+def infill_points(obj_points, infill_type="cross"):
+    """
+    calculating the infill 
+    """
+    infill_points = [] 
+    
+    return infill_points
+    
+if __name__ == '__main__':
+    pass
