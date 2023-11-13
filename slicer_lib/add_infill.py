61fe35e4d6cbd7455635c56bbe464c215c3bc4e7
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2023-11-13 22:07:43 +0100

    Add files via upload

--- slicer_lib/add_infill.py
+++ slicer_lib/add_infill.py
@@ -1,11 +1,52 @@
 from Line import *
 
-def cross_lines(obj_size_x = 10, obj_size_y = 10, obj_size_z = 10, spacing = 0.5):
+def cross_lines(obj_size_x = 10, obj_size_y = 10, obj_size_z = 10, spacing = 0.5, layer_height = 0.2,obj_offset_x = 10, obj_offset_y = 10):
     cross_lines = []
     
-    return cross_lines
+    line_x_count = obj_size_x / spacing
+    line_y_count = obj_size_y / spacing 
     
-def infill_points(obj_points, infill_type="cross"):
+    
+    # creating x lines 
+    for height in range(0, obj_size_z, layer_height):
+        cross_layer_lines = []
+        x_dir_vector = [1, 0, 0]
+        y_dir_vector = [0, 1, 0]
+        for x_value in range(obj_offset_x, obj_offset_x, spacing):
+            support = [x_value, obj_offset_y, height]
+            line = Line(support, x_dir_vector)
+            cross_layer_lines.append(line)
+            
+        for y_value in range(obj_offset_y, obj_size_y, spacing):
+            support = [obj_offset_x, y_value, height]
+            line = Line(support, y_dir_vector)
+            cross_layer_lines.append(line)
+            
+        cross_lines.append(cross_layer_lines)
+
+    return cross_lines
+
+def convert_obj_points_to_line(obj_wall_point_pairs):
+    obj_wall_lines = []
+    
+    for layer in obj_wall_point_pairs:
+        line_layer = []
+        for element in layer:
+            line_element = []
+            for pair in element:
+                the_line = ComplexLine(pair[0], pair[-1])
+                line_element.append(the_line)
+            line_layer.append(line_element)
+    
+    
+def calc_infill_points(obj_wall_lines, infill_lines):
+    # looks at the intersection of an infill line with the walls of the obj
+    
+    pass
+    
+    
+
+def infill_points(obj_wall_point_pairs, infill_type="cross"):
     """
     calculating the infill 
     """
