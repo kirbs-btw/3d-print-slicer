6a7342c86db0574d49244fdebebabcc3ca2beed3
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2023-11-19 19:43:44 +0100

    Add files via upload
    now infill ponts are generated

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from stl import mesh
 from slicer_lib import stl_to_lines as stltl
 from slicer_lib import lines_to_points as ltp
+from slicer_lib import plane_point_pairs as ppp
+from slicer_lib import add_infill as adi
 
 def main():
     printer_x = 2000
@@ -8,7 +10,7 @@ def main():
     printer_z = 2000
     
     file_path = ''
-    layer_hight = 0.2 # in mm
+    layer_height = 0.2 # in mm
     obj_x_dim = 50 # in mm
     obj_y_dim = 50 # in mm
     obj_z_dim = 50 # in mm
@@ -21,7 +23,27 @@ def main():
 
     stl_obj = mesh.Mesh.from_file(file_path)
     line_triangles = stltl.get_points_from_stl(stl_obj, obj_x_dim=obj_x_dim, obj_y_dim=obj_y_dim, obj_z_dim=obj_z_dim, x_plate_offset=x_plate_offset, y_plate_offset=y_plate_offset)
-    points = ltp.lines_to_points(line_triangles, layer_hight)
+    
+    # wall points
+    # format: new format is:[points[layer[element[pair[points]]]]]
+    obj_layer_point_pairs = ltp.lines_to_points(line_triangles, layer_height)
+    
+    
+    
+    # infill_point in pair form
+    infill_points = adi.infill_points(obj_layer_point_pairs, "cross", obj_x_dim, obj_y_dim, obj_z_dim, spacing=0.5, layer_height=layer_height, obj_offset_x=x_plate_offset, obj_offset_y=y_plate_offset)
+    
+    # combine points
+    
+    
+    
+    
+    
+    # reformate the point pairs to points 
+    # reformat
+    # format: new format is:[points[layer[element[points]]]]
+    obj__points = ppp.plane_pairs(obj_layer_point_pairs)
+    
     
 if __name__ == '__main__':
     main()
