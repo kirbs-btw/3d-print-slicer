d343d073fc4243c5e2e244e047afb8262a46ab66
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2023-11-22 20:05:44 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from slicer_lib import lines_to_points as ltp
 from slicer_lib import plane_point_pairs as ppp
 from slicer_lib import add_infill as adi
 from slicer_lib import combine_parts as cop
+from slicer_lib import points_to_gcode as ptg
+from slicer_lib import gcode_to_file as gtf
 
 def main():
     printer_x = 2000
@@ -19,8 +21,8 @@ def main():
     x_plate_offset = 50
     y_plate_offset = 50
 
-    file_name = ''
-    save_path = ''
+    file_name = 'test.gcode'
+    save_path = 'H:/Projekte/Projekte/Project 137/slicer_2/out/'
 
     stl_obj = mesh.Mesh.from_file(file_path)
     line_triangles = stltl.get_points_from_stl(stl_obj, obj_x_dim=obj_x_dim, obj_y_dim=obj_y_dim, obj_z_dim=obj_z_dim, x_plate_offset=x_plate_offset, y_plate_offset=y_plate_offset)
@@ -29,10 +31,11 @@ def main():
     # format: new format is:[points[layer[element[pair[points]]]]]
     obj_layer_point_pairs = ltp.lines_to_points(line_triangles, layer_height)
     
-    
-    
     # infill_point in pair form
     infill_point_pairs = adi.infill_points(obj_layer_point_pairs, "cross", obj_x_dim, obj_y_dim, obj_z_dim, spacing=0.5, layer_height=layer_height, obj_offset_x=x_plate_offset, obj_offset_y=y_plate_offset)
+    
+    # fill bottom and top
+
     
     # combine points
     obj_point_pairs = cop.combine_pairs(obj_layer_point_pairs, infill_point_pairs)
@@ -44,6 +47,11 @@ def main():
     
     # ...
     # slicing to gcode 
+    gcode = ptg.create_gcode(obj_points)
+    
+    gtf.gcdoe_to_file(gcode, file_name, save_path)
+    
+    
     
 if __name__ == '__main__':
     main()
