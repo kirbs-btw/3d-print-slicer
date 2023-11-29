9ef8f91a6e2499d6daa6912901aa33d7b8d53590
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2023-11-29 21:21:45 +0100

    Add files via upload

--- slicer_lib/gcode_to_file.py
+++ slicer_lib/gcode_to_file.py
@@ -2,5 +2,6 @@ def gcode_to_file(gcode, filename, path):
     full_path =  str(path) + str(filename)
     f = open(full_path, "w+")
     for line in gcode:
-        f.writelines(line)
+        line_txt = "{}\n".format(line)
+        f.writelines(line_txt)
     f.close()
