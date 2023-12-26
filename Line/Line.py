9a8261358199719d75ac1e7edc46f30ee2fd9c24
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2023-12-26 22:04:39 +0100

    Add files via upload
    Line bug fix to calc points

--- Line/Line.py
+++ Line/Line.py
@@ -21,9 +21,9 @@ class Line:
         print('----------------')
     
     def point(self, s):
-        x1 = self.dV[0] + self.sV[0] * s
-        x2 = self.dV[1] + self.sV[1] * s
-        x3 = self.dV[2] + self.sV[2] * s
+        x1 = self.sV[0] + self.dV[0] * s
+        x2 = self.sV[1] + self.dV[1] * s
+        x3 = self.sV[2] + self.dV[2] * s
         return [x1, x2, x3]
 
     def info(self):
