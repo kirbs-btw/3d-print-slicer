9c4e01b5ef3429915ec1c78eaf99e9465988f63d
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2023-12-29 19:28:05 +0100

    testing
    testing a hardcoded way to see where the error is

--- slicer_lib/combine_parts.py
+++ slicer_lib/combine_parts.py
@@ -1,3 +1,23 @@
+
+# points form is points = [layer[element[point[]]]]
+# combining the elements of the other array to the new one
+# combination issue ?
+
+
+def combine_pairs(obj_points, infill_points):
+    new_points = []
+    for idx, layer in enumerate(obj_points):
+        new_layer = []
+        for element in layer:
+            new_layer.append(element)
+        for element in infill_points[idx]:
+            new_layer.append(element)
+        new_points.append(new_layer)
+    return new_points
+
+
+"""
+
 def combine_pairs(*args):
     # defines the first arg as a scaffold to append the other elements in 
     obj_point_pairs = args[0]
@@ -13,4 +33,4 @@ def combine_pairs(*args):
                 obj_point_pairs[layer_index].append(element)
     
     return obj_point_pairs
-    
+"""
